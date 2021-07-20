c0757af2edb0e0d07cdf31a144159a05e35d0ba4
Hilton1 <josehilton02@gmail.com>
2021-07-19 21:21:45 -0300

    First commit

---
+++ src/mocks/users.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Hilton'
+  },
+  {
+    id: 2,
+    name: 'Alisson'
+  },
+  {
+    id: 3,
+    name: 'Mizael'
+  },
+  {
+    id: 4,
+    name: 'Junior'
+  }
+];
